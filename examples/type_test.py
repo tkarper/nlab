ce244c54617cf144dbdce752beec3a3aa7e97010
tkarper <karper@gmail.com>
2014-02-11 14:54:53 +0100

    En oppdatering

---
+++ examples/type_test.py
@@ -0,0 +1,13 @@
+import sys
+sys.path.append('../')
+
+from numpy import *
+from nlab import *
+import matplotlib.pyplot as plt
+
+
+L = array([Neuron_IF() for _ in range(0,4)])
+for i in range(0,4):
+	L[i].sp = i
+	
+connect_one_to_many(L[0], L, 1.0)
